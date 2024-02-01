34d61645e97f099cc7a3e79e9b8b2c467a31b16e
BillyTilly <donottouchme@inbox.ru>
2024-02-01 13:30:50 +0300

    iter1 is done

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,67 @@
 package main
 
-func main() {}
+import (
+	"io"
+	"math/rand"
+	"net/http"
+)
+
+var host = "localhost:8080"
+
+var a = make(map[string]string)
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		n := r.Header.Get("Content-type")
+		body, _ := io.ReadAll(r.Body)
+
+		if "text/plain" != n {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		key := generateKey()
+		a[key] = string(body)
+
+		answer := "http://localhost:8080/" + key
+
+		w.Header().Set("Content-type", "text/plain")
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(answer))
+
+		return
+	} else {
+		key := r.URL.Path[len("/"):]
+		if key == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		res, ok := a[key]
+		if ok {
+			w.Header().Set("Location", res)
+			w.WriteHeader(http.StatusTemporaryRedirect)
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+
+	}
+}
+
+func generateKey() string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	const keyLength = 6
+
+	shortKey := make([]byte, keyLength)
+	for i := range shortKey {
+		shortKey[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(shortKey)
+}
+
+func main() {
+	err := http.ListenAndServe(host, http.HandlerFunc(handle))
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/BillyTilly/yndxpract
+
+go 1.20
